a91fb81893d73dc36881a0c0641c3e440f2ab006
jiachengnlong <jiachenglong@ybm100.com>
2021-07-20 11:25:35 +0800

    请你在最优数对划分的方案下，返回最小的 最大数对和

---
+++ minPairSum.js
@@ -0,0 +1,27 @@
+/**
+ * 一个数对 (a,b) 的 数对和 等于 a + b 。最大数对和 是一个数对数组中最大的 数对和 。
+ * 比方说，如果我们有数对 (1,5) ，(2,3) 和 (4,4)，最大数对和 为 max(1+5, 2+3, 4+4) = max(6, 5, 8) = 8 。
+ * 给你一个长度为 偶数 n 的数组 nums ，请你将 nums 中的元素分成 n / 2 个数对，使得：
+
+ * nums 中每个元素 恰好 在 一个 数对中，且
+ * 最大数对和 的值 最小 。
+ * 请你在最优数对划分的方案下，返回最小的 最大数对和 
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minPairSum = function(nums) {
+  nums = nums.sort((a,b)=>a-b)
+  var max = 0;
+  var left = 0, right = nums.length-1;
+  for(left = 0; left < right ; left++ ){
+    max = Math.max(nums[left]+ nums[right] , max);
+    right--;
+  }
+  return max
+
+};
+
+var a = [4,1,5,1,2,5,1,5,5,4];
+minPairSum(a)
