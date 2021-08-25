679e4109dcc0f436dde9549ef58257e40011203e
jiachengnlong <251895921@qq.com>
2021-08-25 18:05:14 +0800

    请你找出所有从节点 0 到节点 n-1 的路径并输出 dfs

--- allPathsSourceTarget.js
+++ allPathsSourceTarget.js
@@ -1,4 +1,11 @@
 /**
+ * 
+ * 给你一个有 n 个节点的 有向无环图（DAG），请你找出所有从节点 0 到节点 n-1 的路径并输出（不要求按特定顺序）
+   二维数组的第 i 个数组中的单元都表示有向图中 i 号节点所能到达的下一些节点，空就是没有下一个结点了。
+   来源：力扣（LeetCode）
+   链接：https://leetcode-cn.com/problems/all-paths-from-source-to-target
+ */
+/**
  * @param {number[][]} graph
  * @return {number[][]}
  */
